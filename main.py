a0dd148a2959ee938f046e24306097e4a4bbb2a7
Exohspoer <kakashihatake6hokagegygg@gmail.com>
2025-12-27 02:14:41 +0200

    Write get user after login

--- main.py
+++ main.py
@@ -17,13 +17,14 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
-hash_password = PasswordHash.recommended()
+password_hash = PasswordHash.recommended()
 
 def hashed_password(password):
-    return hash_password.hash(password)
+    return password_hash.hash(password)
 
 def verify_password(plain_password, hashed_password):
-    return hash_password.verify(plain_password, hashed_password)
+    return password_hash.verify(plain_password, hashed_password)
+
 
 @app.post('/token')
 async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
@@ -39,7 +40,10 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = 
     
     access_token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITM)
     
-    return Token(token=access_token, access_type='bearer')
+    return {
+    "access_token": access_token,
+    "token_type": "bearer"
+}
 
 @app.post('/register', response_model=UserResponse, status_code=201)
 async def register(user: CreateUser, db: Session = Depends(get_db)):
@@ -48,12 +52,39 @@ async def register(user: CreateUser, db: Session = Depends(get_db)):
     if exiting_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='User with this username or email already exists')
     
-    hash_password = hashed_password(user.password)
+    new_hash_password = hashed_password(user.password)
     
-    new_user = Users(username=user.username, email=user.email, password=hash_password)
+    new_user = Users(username=user.username, email=user.email, password=new_hash_password)
     
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     
     return new_user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> Users:
+    creditials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},)
+    
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITM])
+        username: str | None = payload.get('sub')
+        
+        if username is None:
+            raise creditials_exception
+        
+    
+    except jwt.InvalidTokenError:
+        raise creditials_exception
+    
+    user = db.query(Users).filter(Users.username == username).first()
+    
+    if user is None:
+        raise creditials_exception
+    
+    return user
+
+@app.get('/user', response_model=UserResponse)
+def read_user(user: Users = Depends(get_current_user)):
+    return user
